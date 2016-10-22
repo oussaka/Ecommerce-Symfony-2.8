803236d3ea7e21a8277e3dc8a28ad403c2bc6583
Etienne <etienne.fontaine1@gmail.com>
2016-10-22 15:34:11 +0200

    Ajout d'une page pour tester la création de formulaire

--- src/EcommerceBundle/Controller/TestController.php
+++ src/EcommerceBundle/Controller/TestController.php
@@ -2,15 +2,15 @@
 
 namespace EcommerceBundle\Controller;
 
-use EcommerceBundle\EcommerceBundle;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use EcommerceBundle\Entity\Produits;
+use Symfony\Component\HttpFoundation\Request;
+use EWZ\Bundle\RecaptchaBundle\Form\Type\EWZRecaptchaType;
 
 class TestController extends Controller
 {
     /**
-     * @Route("/test", name="test")
+     * @Route("/testAddBdd", name="testAddBdd")
      */
     public function ajoutAction()
     {
@@ -42,6 +42,39 @@ class TestController extends Controller
         $produits = $em->getRepository('EcommerceBundle:Produits')->findAll();
 
         // Affichage sur la page
-        return $this->render('EcommerceBundle:Front/Test:index.html.twig', array('titre' => 'Les produits', 'produits' => $produits));
+        return $this->render('EcommerceBundle:Front/Test:testAddBdd.html.twig', array('titre' => 'Les produits', 'produits' => $produits));
+    }
+
+    /**
+     * @Route("/testFormulaire", name="testFormulaire")
+     */
+    public function testFormulaireAction(Request $request)
+    {
+        $form = $this->createFormBuilder()
+                    ->add('Email', 'email')
+                    ->add('Nom','text', array('required' => false))
+                    ->add('Genre', 'choice', array('required' => false, 'choices' => array('0' => 'Homme',
+                        '1' => 'Femme',
+                        '2' => 'Autre')))
+                    ->add('Cheveux', 'choice', array('required' => false, 'choices' => array('0' => 'Brun',
+                        '1' => 'Blond',
+                        '2' => 'Chatain'), 'expanded' => true))
+                    ->add('Message', 'textarea', array('required' => false))
+                    ->add('Date', 'date', array('required' => false))
+                    ->add('Utilisateurs', 'entity', array('class' => 'UtilisateursBundle\Entity\Utilisateurs'), array('required' => false))
+                    ->add('Pays', 'country', array('preferred_choices' => array('FR')), array('required' => false))
+                    ->add('Recaptcha', EWZRecaptchaType::class, array('required' => false))
+                    ->add('Envoyer', 'submit', array('attr' => array('class' => 'btn btn-block btn-info')), array('required' => false))
+                    ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            echo $form['Email']->getData();
+        }
+
+
+        return $this->render('@Ecommerce/Front/Test/testFormulaire.html.twig', array('titre' => 'Contact','form' => $form->createView()));
     }
 }
